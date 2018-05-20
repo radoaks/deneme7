216cc56f1ad03f33630c7be5ee6e3c645add8432
Rıdvan Aksoy <ridvanaksoy@hotmail.com.tr>
2018-05-20 18:20:18 +0300

    deneme eklendi

--- YMS3514GithubDenem/YMS3514GithubDenem/Form1.cs
+++ YMS3514GithubDenem/YMS3514GithubDenem/Form1.cs
@@ -20,6 +20,7 @@ namespace YMS3514GithubDenem
         private void Form1_Load(object sender, EventArgs e)
         {
             MessageBox.Show("Test");
+            MessageBox.Show("Deneme");
         }
     }
 }
